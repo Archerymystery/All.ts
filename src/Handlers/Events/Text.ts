7de936e5dbd3615ed080fe3232dcb94692af447b
Archermystery <ratibor200805@gmail.com>
2023-12-22 01:21:10 +0200

    create download tiktok music

--- src/Handlers/Events/Text.ts
+++ src/Handlers/Events/Text.ts
@@ -1,8 +1,10 @@
-import { Context, Telegraf } from "telegraf";
+import { Context, Markup, Telegraf } from "telegraf";
 import { Handler } from "../Handler";
 import { TiktokDL } from "@tobyg74/tiktok-api-dl"
-import type { Update, Message, InputMediaPhoto } from "telegraf/types";
+import type { Update, Message, InputMediaPhoto, CallbackQuery } from "telegraf/types";
+
 import { MusicalDownResponse } from "@tobyg74/tiktok-api-dl/lib/types/musicaldown";
+import { MatchedMiddleware } from "telegraf/typings/composer";
 export default class Text extends Handler {
   constructor(bot: Telegraf) {
     super(bot)
@@ -13,19 +15,41 @@ export default class Text extends Handler {
 
     }).then((result) => {
       const musicalResult = result as MusicalDownResponse;
+      const kayBoard = Markup.inlineKeyboard([
+        Markup.button.callback("Download", `music:${ctx.message.text}`)
+      ]);
+      if (musicalResult.status === "error") {
+        ctx.reply("This video is private or remove")
+      }
       if (result.result?.type === "video" && musicalResult.result?.video1) {
-        ctx.replyWithVideo({ url: musicalResult.result?.video1 });
+        ctx.replyWithVideo({ url: musicalResult.result?.video1 }).then(() => {
+          ctx.reply("Music", kayBoard)
+        });;
       } else if (result.result?.type === "image") {
         const groop: InputMediaPhoto[] = []
         result.result.images?.forEach((item) => {
           groop.push({ type: "photo", media: { url: item } })
+        });
+        ctx.replyWithMediaGroup(groop, {}).then(() => {
+          ctx.reply("Music", kayBoard)
         })
-        ctx.replyWithMediaGroup(groop)
+
       }
+
     })
   }
   handler(): void {
     console.log("Text")
+    this.bot.action(/music:(.+)/, async (ctx) => {
+      TiktokDL(ctx.match[1], {
+        version: "v3"
+      }).then((result) => {
+        const musicalResult = result as MusicalDownResponse
+        if (musicalResult.result?.music) {
+          ctx.replyWithAudio({ url: musicalResult.result?.music })
+        }
+      })
+    });
     this.bot.on('text', async (ctx: Context<Update.MessageUpdate<Message.TextMessage>>) => {
 
       if (ctx.message.text.startsWith("https://www.tiktok.com/")
