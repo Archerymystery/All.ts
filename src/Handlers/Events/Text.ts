5a386ddc7eac1bac55312510da1e57519675f7c5
Archermystery <ratibor200805@gmail.com>
2024-07-27 23:32:30 +0300

    Changing the downloader structure

--- src/Handlers/Events/Text.ts
+++ src/Handlers/Events/Text.ts
@@ -1,93 +1,19 @@
 import { Context, Telegraf } from "telegraf";
 import { Handler } from "../Handler";
-import { Downloader } from "@tobyg74/tiktok-api-dl"
-import type { Update, Message, InputMediaPhoto } from "telegraf/types";
-import { TiktokAPIResponse } from "@tobyg74/tiktok-api-dl/lib/types/downloader/tiktokApi";
-import logger from "../../logger";
+import type { Update, Message } from "telegraf/types";
+import sendTiktoks from "../../Downloders/Tiktok";
 export default class Text extends Handler {
   constructor(bot: Telegraf) {
     super(bot)
   }
-  sendTiktoks(ctx: Context<Update.MessageUpdate<Message.TextMessage>>, urls: string[]): void {
-    if (urls.length == 0) return
-    Downloader(urls[0], {
-      version: "v1"
 
-    }).then((result: TiktokAPIResponse) => {
-      logger.info(`Start sending tiktok: ${urls[0]} for user: @${ctx.message.from.username} chat id: ${ctx.message.chat.id}  `)
-      if (result.status === "error") {
-        ctx.reply("This video is private or remove")
-        urls.shift()
-        this.sendTiktoks(ctx, urls)
-
-      }
-      if (
-        result.result?.type === "video" &&
-        result.result.video?.playAddr[0]
-      ) {
-        ctx.sendVideo(
-          {
-            url: result.result.video.playAddr[0],
-          },
-          {
-            caption:
-              `${result.result.description}\n[${result.result.author.nickname}](https://www.tiktok.com/@${result.result.author.nickname})`,
-            parse_mode: 'Markdown'
-          }).then(() => {
-            if (result.result?.music) {
-              ctx.replyWithAudio(
-                {
-                  url: result.result.music.playUrl[0],
-
-                },
-                {
-                  thumbnail: { url: result.result.music.coverThumb[0] },
-                  performer: result.result.music.author,
-                  title: result.result.music.title,
-                  duration: result.result.music.duration,
-                }
-              ).then(() => {
-                urls.shift()
-                this.sendTiktoks(ctx, urls)
-              })
-            }
-          });
-      } else if (result.result?.type === "image" && result.result?.images) {
-        const groop: InputMediaPhoto[] = []
-
-        result.result?.images.forEach((item) => {
-          groop.push({ type: "photo", media: { url: item } })
-        });
-        ctx.replyWithMediaGroup(groop, {}).then(() => {
-          if (result.result?.music && result.result.music) {
-            ctx.replyWithAudio(
-              {
-                url: result.result.music.playUrl[0],
-
-              },
-              {
-                thumbnail: { url: result.result.music.coverThumb[0] },
-                performer: result.result.music.author,
-                title: result.result.music.title,
-                duration: result.result.music.duration,
-              }
-            ).then(() => {
-              urls.shift()
-              this.sendTiktoks(ctx, urls)
-            })
-          }
-        })
-      }
-
-    })
-  }
   handler(): void {
     console.log("Text")
     this.bot.on('text', async (ctx: Context<Update.MessageUpdate<Message.TextMessage>>) => {
       const regexTiktok = /https:\/\/vm.tiktok.com\/[a-zA-Z1-9]+|https:\/\/www.tiktok.com\/@[a-zA-Z1-9_.]+\/video\/[0-9]+|https:\/\/www.tiktok.com\/@[a-zA-Z1-9_.]+\/photo\/[0-9]+/gm;
       let tiktokUrls = ctx.message.text.match(regexTiktok)
       if (tiktokUrls) {
-        this.sendTiktoks(ctx, tiktokUrls)
+        sendTiktoks(ctx, tiktokUrls)
       }
     })
   }
