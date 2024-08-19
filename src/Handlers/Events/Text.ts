904ddc108f9639f2ae69a5dde2d3aac4951ff509
Archerymystery <84961834+Archerymystery@users.noreply.github.com>
2024-08-20 00:38:19 +0300

    refotmat text handler

--- src/Handlers/Events/Text.ts
+++ src/Handlers/Events/Text.ts
@@ -2,6 +2,7 @@ import { Context, Telegraf } from "telegraf";
 import { Handler } from "../Handler";
 import type { Update, Message } from "telegraf/types";
 import sendTiktoks from "../../Downloders/Tiktok";
+const regexTiktok = /https:\/\/vm.tiktok.com\/[a-zA-Z1-9]+|https:\/\/www.tiktok.com\/@[a-zA-Z1-9_.]+\/video\/[0-9]+|https:\/\/www.tiktok.com\/@[a-zA-Z1-9_.]+\/photo\/[0-9]+/gm;
 export default class Text extends Handler {
   constructor(bot: Telegraf) {
     super(bot)
@@ -10,7 +11,6 @@ export default class Text extends Handler {
   handler(): void {
     console.log("Text")
     this.bot.on('text', async (ctx: Context<Update.MessageUpdate<Message.TextMessage>>) => {
-      const regexTiktok = /https:\/\/vm.tiktok.com\/[a-zA-Z1-9]+|https:\/\/www.tiktok.com\/@[a-zA-Z1-9_.]+\/video\/[0-9]+|https:\/\/www.tiktok.com\/@[a-zA-Z1-9_.]+\/photo\/[0-9]+/gm;
       let tiktokUrls = ctx.message.text.match(regexTiktok)
       if (tiktokUrls) {
         sendTiktoks(ctx, tiktokUrls)
