791515110f526f302a952876c461c8f596bc3e0b
Archerymystery <84961834+Archerymystery@users.noreply.github.com>
2024-08-20 00:49:21 +0300

    Add start log in Text handler

--- src/Handlers/Events/Text.ts
+++ src/Handlers/Events/Text.ts
@@ -2,6 +2,7 @@ import { Context, Telegraf } from "telegraf";
 import { Handler } from "../Handler";
 import type { Update, Message } from "telegraf/types";
 import sendTiktoks from "../../Downloders/Tiktok";
+import logger from "../../logger";
 const regexTiktok = /https:\/\/vm.tiktok.com\/[a-zA-Z1-9]+|https:\/\/www.tiktok.com\/@[a-zA-Z1-9_.]+\/video\/[0-9]+|https:\/\/www.tiktok.com\/@[a-zA-Z1-9_.]+\/photo\/[0-9]+/gm;
 export default class Text extends Handler {
   constructor(bot: Telegraf) {
@@ -9,7 +10,7 @@ export default class Text extends Handler {
   }
 
   handler(): void {
-    console.log("Text")
+    logger.info("Handler Text is on")
     this.bot.on('text', async (ctx: Context<Update.MessageUpdate<Message.TextMessage>>) => {
       let tiktokUrls = ctx.message.text.match(regexTiktok)
       if (tiktokUrls) {
